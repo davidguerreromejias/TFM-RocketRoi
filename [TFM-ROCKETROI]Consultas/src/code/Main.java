81014cfff9727a6ba6967000b99cd220b3335f3b
davidguerreromejias <davidguerreromejias@gmail.com>
2018-06-06 20:34:42 +0200

    Prueba nuevo repo.

--- [TFM-ROCKETROI]Consultas/src/code/Main.java
+++ [TFM-ROCKETROI]Consultas/src/code/Main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
+		
 		ReadFile ReadFile = new ReadFile(args[0]);
 		System.out.print(ReadFile);
 	}
