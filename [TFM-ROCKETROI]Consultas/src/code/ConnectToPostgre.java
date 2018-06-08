bf096417ea330db02eba4f0d5bd97b240cbb150c
Aleix Che <Aleix Che@Aleix-PC>
2018-06-08 20:08:43 +0200

    - Afegit al pom la depencia per el driver jdbc postgre. - Creada la clase ConnectToPostgre - Manipulat el Main ( lo que David va fer esta comentat al principi).

--- [TFM-ROCKETROI]Consultas/src/code/ConnectToPostgre.java
+++ [TFM-ROCKETROI]Consultas/src/code/ConnectToPostgre.java
@@ -1,5 +1,34 @@
 package code;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.datastax.driver.core.Session;
+
 public class ConnectToPostgre {
+	
+	Session session;
+	Connection conn;
+	
+	
+	public ConnectToPostgre(String ip) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated constructor stub
+		String User = "postgres";
+		String Password = "lasalle18";
+		String db = "postgres";
+		String Port = "5432";
+		Class.forName("org.postgresql.Driver");
+		
+		String url = "jdbc:postgresql://"+ip+":"+Port+"/"+db;
+		Properties props = new Properties();
+		props.setProperty("user",User);
+		props.setProperty("password",Password);
+		props.setProperty("ssl","false");
+		conn = DriverManager.getConnection(url, props);
+
+		
+	}
 
 }
--- [TFM-ROCKETROI]Consultas/src/code/Main.java
+++ [TFM-ROCKETROI]Consultas/src/code/Main.java
@@ -8,9 +8,26 @@ public class Main {
 		
 		//ReadFile ReadFile = new ReadFile(args[0]);
 		//System.out.print(ReadFile);
-		ConnectToCassandra ctc = new ConnectToCassandra(args[0], args[1]);
-		ctc.readToCassandra();
+		/*ConnectToCassandra ctc = new ConnectToCassandra(args[0], args[1]);
+		ctc.readToCassandra();*/
 		//SparkToCassandra stc = new SparkToCassandra();
+		
+	      
+	      try {
+	         //Class.forName("org.postgresql.Driver");
+	         ConnectToPostgre c = new ConnectToPostgre("192.168.1.36");
+	         
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         System.err.println(e.getClass().getName()+": "+e.getMessage());
+	         System.exit(0);
+	      }
+	      System.out.println("Opened database successfully");
+	   
+		
+		
+		
+		
 	}
 
 }
