55f619dac80d63fa3e759463c43da4b717f3e848
Aleix Che <apaleix@gmail.com>
2018-06-16 15:52:35 +0200

    Se ha creado una nueva clase para realizar reintentos a la hora de escribir en cassandra

--- [TFM-ROCKETROI]Consultas/src/code/ConnectToCassandra.java
+++ [TFM-ROCKETROI]Consultas/src/code/ConnectToCassandra.java
@@ -8,6 +8,7 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.policies.RetryPolicy;
 
 public class ConnectToCassandra {
 
@@ -15,10 +16,11 @@ public class ConnectToCassandra {
 	
 	public ConnectToCassandra(String ip, String keyspace) {
 		// TODO Auto-generated constructor stub
-
+		RetryPolicy rc = new CustomRetryPolicy(3, 3, 2);
 		Cluster cluster = Cluster.builder()
 		  .addContactPoints(ip)
 		  .withCredentials("cassandra", "cassandra")
+		  .withRetryPolicy(rc)
 		  .build();
 		
 		ProtocolVersion myCurrentVersion = cluster.getConfiguration().getProtocolOptions().getProtocolVersion();
@@ -38,6 +40,9 @@ public class ConnectToCassandra {
 		//String cqlStatement = "INSERT INTO accountid_campaign_id (account_id,date_short,ad_group_id,keyword_id,date,campaign_id,avg_cpc,avg_position,bid,clicks,conversion,cost,cpc_max,ctr,first_page_cpc,impressions,keyword_text,keyword_type,quality,status,top_of_page_cpc,all_conversions,conversion_value,search_impression_share,search_rank_lost_impression_share,all_conversion_value,avg_cost,avg_cpe,bounce_rate,click_assisted_conversions,click_assisted_conversions_over_last_click_conversions,click_assisted_conversion_value,cross_device_conversions,first_position_cpc,search_exact_match_impression_share,value_per_all_conversion,value_per_conversion) VALUES (1,'20171209',48321378399,439647333247,'1512777600000',991542199,01,1,NULL,1,0,01,NULL,50,05,2,'tecnolytic','EXACT',10,'ENABLED',0,0,0,100,0,0,01,0,0,0,0,0,0,05,100,0,0);";
 		session.execute(cqlStatement);
 		//session.close();
+	}
+	public void closeCassandra(){
+		session.close();
 	}
 	
 }
