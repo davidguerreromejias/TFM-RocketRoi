b507c7433a78ed38f686cb21d9c54087d1409ecd
Aleix Che <apaleix@gmail.com>
2018-06-16 12:34:04 +0200

    se ha conseguido realizar el insert en cassandra

--- [TFM-ROCKETROI]Consultas/src/code/ConnectToCassandra.java
+++ [TFM-ROCKETROI]Consultas/src/code/ConnectToCassandra.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
@@ -19,7 +20,9 @@ public class ConnectToCassandra {
 		  .addContactPoints(ip)
 		  .withCredentials("cassandra", "cassandra")
 		  .build();
-
+		
+		ProtocolVersion myCurrentVersion = cluster.getConfiguration().getProtocolOptions().getProtocolVersion();
+		//System.out.println(myCurrentVersion.getClass().getName());
 		session = cluster.connect(keyspace);
 	}
 	
@@ -30,11 +33,11 @@ public class ConnectToCassandra {
 		} 
 	}
 	
-	public void importToCassandra() throws FileNotFoundException{
+	public void importToCassandra(String cqlStatement) throws FileNotFoundException{
 		
-		String cqlStatement = "INSERT INTO accountid_campaign_id (account_id,date_short,ad_group_id,keyword_id,date,campaign_id,avg_cpc,avg_position,bid,clicks,conversion,cost,cpc_max,ctr,first_page_cpc,impressions,keyword_text,keyword_type,quality,status,top_of_page_cpc,all_conversions,conversion_value,search_impression_share,search_rank_lost_impression_share,all_conversion_value,avg_cost,avg_cpe,bounce_rate,click_assisted_conversions,click_assisted_conversions_over_last_click_conversions,click_assisted_conversion_value,cross_device_conversions,first_position_cpc,search_exact_match_impression_share,value_per_all_conversion,value_per_conversion) VALUES (1,'20171209',48321378399,439647333247,'1512777600000',991542199,01,1,NULL,1,0,01,NULL,50,05,2,'tecnolytic','EXACT',10,'ENABLED',0,0,0,100,0,0,01,0,0,0,0,0,0,05,100,0,0);";
+		//String cqlStatement = "INSERT INTO accountid_campaign_id (account_id,date_short,ad_group_id,keyword_id,date,campaign_id,avg_cpc,avg_position,bid,clicks,conversion,cost,cpc_max,ctr,first_page_cpc,impressions,keyword_text,keyword_type,quality,status,top_of_page_cpc,all_conversions,conversion_value,search_impression_share,search_rank_lost_impression_share,all_conversion_value,avg_cost,avg_cpe,bounce_rate,click_assisted_conversions,click_assisted_conversions_over_last_click_conversions,click_assisted_conversion_value,cross_device_conversions,first_position_cpc,search_exact_match_impression_share,value_per_all_conversion,value_per_conversion) VALUES (1,'20171209',48321378399,439647333247,'1512777600000',991542199,01,1,NULL,1,0,01,NULL,50,05,2,'tecnolytic','EXACT',10,'ENABLED',0,0,0,100,0,0,01,0,0,0,0,0,0,05,100,0,0);";
 		session.execute(cqlStatement);
-		session.close();
+		//session.close();
 	}
 	
 }
--- [TFM-ROCKETROI]Consultas/src/code/Main.java
+++ [TFM-ROCKETROI]Consultas/src/code/Main.java
@@ -3,6 +3,7 @@ package code;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.apache.cassandra.thrift.Cassandra.AsyncProcessor.system_add_column_family;
 
@@ -11,32 +12,45 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		
-		String allColumnsPath = args[0]+"\\keyword 2_1.csv";
+		String allColumnsPath = args[0]+"\\campana2_1.csv";
 		String AdGroupPath = args[0]+"\\Sequencia.csv";
 		System.out.println(allColumnsPath);
 		
 		CreateData d =  new CreateData(allColumnsPath);
 		
 		ArrayList<String[]> g = d.getNewData();
-		String[] s =d.getNewDataAd(3, AdGroupPath);
+		System.out.println(g.size());
+		String[] s =d.getNewDataAd(5, AdGroupPath);
 		
-		/*for (int i=0; i<s.length; i++){
-			System.out.println(s[i]+";");
-			
-		}*/
+		
+		ConnectToCassandra ctc = new ConnectToCassandra(args[2], "campaign");
+		String table = "AccountID_campaign_ID";
+		int dataindex=1;
+		
+		Date startDate = new Date();
+        for( int i=0; i <s.length; i++){
+	    	  
+		      String Consulta = "INSERT INTO "+table+"( "+g.get(0)[0]+", "+g.get(0)[1]+", "+g.get(0)[2]+", "+g.get(0)[3]+", "+g.get(0)[4]+", "+g.get(0)[5]+", "+g.get(0)[6]+", "+g.get(0)[7]+", "+g.get(0)[8]+", "+g.get(0)[9]+", "+g.get(0)[10]+", "+g.get(0)[11]+", "+g.get(0)[12]+", "+g.get(0)[13]+", "+g.get(0)[14]+", "+g.get(0)[15]+", "+g.get(0)[16]+", "+g.get(0)[17]+", "+g.get(0)[18]+", "+g.get(0)[19]+", "+g.get(0)[20]+", "+g.get(0)[21]+", "+g.get(0)[22]+", "+g.get(0)[23]+", "+g.get(0)[24]+", "+g.get(0)[25]+", "+g.get(0)[26]+", "+g.get(0)[27]+", "+g.get(0)[28]+", "+g.get(0)[29]+", "+g.get(0)[30]+", "+g.get(0)[31]+", "+g.get(0)[32]+", "+g.get(0)[33]+", "+g.get(0)[34]+", "+g.get(0)[35]+", "+g.get(0)[36]+")VALUES ( "+g.get(dataindex)[0]+", '"+g.get(dataindex)[1]+"', "+s[i]+", "+g.get(dataindex)[3]+", '"+g.get(dataindex)[4]+"', "+g.get(dataindex)[5]+", "+g.get(dataindex)[6]+", "+g.get(dataindex)[7]+", "+g.get(dataindex)[8]+", "+g.get(dataindex)[9]+", "+g.get(dataindex)[10]+", "+g.get(dataindex)[11]+", "+g.get(dataindex)[12]+", "+g.get(dataindex)[13]+", "+g.get(dataindex)[14]+", "+g.get(dataindex)[15]+", '"+g.get(dataindex)[16]+"', '"+g.get(dataindex)[17]+"', "+g.get(dataindex)[18]+", '"+g.get(dataindex)[19]+"', "+g.get(dataindex)[20]+", "+g.get(dataindex)[21]+", "+g.get(dataindex)[22]+", "+g.get(dataindex)[23]+", "+g.get(dataindex)[24]+", "+g.get(dataindex)[25]+", "+g.get(dataindex)[26]+", "+g.get(dataindex)[27]+", "+g.get(dataindex)[28]+", "+g.get(dataindex)[29]+", "+g.get(dataindex)[30]+", "+g.get(dataindex)[31]+", "+g.get(dataindex)[32]+", "+g.get(dataindex)[33]+", "+g.get(dataindex)[34]+", "+g.get(dataindex)[35]+", "+g.get(dataindex)[36]+");";
+		      //System.out.println(Consulta);
+		      ctc.importToCassandra(Consulta);
+		      dataindex++;
+		      if(dataindex== g.size()){
+		    	  dataindex=1;
+		      }
+		      
+        }
+        Date endDate = new Date();
+        
+        int msElapsedTime = (int) (endDate.getTime() - startDate.getTime());
+        ctc.session.close();
+        System.out.println("Inserted Succesfully in "+ msElapsedTime+" ms");
 		
 		
 		/*ConnectToCassandra ctc = new ConnectToCassandra(args[0], args[1]);
 		ctc.importToCassandra();*/
 		//SparkToCassandra stc = new SparkToCassandra();
 		
-		
-		
-		/*String Path = "C:\\Users\\Aleix Che\\TFM\\Prueba 2_0\\keyword_example.csv";
-		ReadFile ReadFile = new ReadFile(Path);
-		System.out.print(ReadFile);*/
-		
-		
+
 		
 		//////////////////////////////REALIZAR CONSULTA DE LECTURA A POSTGRE//////////////////////////////////////////////
 		
@@ -59,7 +73,7 @@ public class Main {
 		
 		
 //////////////////////////////REALIZAR CONSULTA DE ESCRITURA A POSTGRE//////////////////////////////////////////////
-			String table = "public.keyword";
+			/*String table = "public.keyword";
 			int dataindex=1;
 	      
 		      //System.out.println(Consulta);
@@ -85,7 +99,7 @@ public class Main {
 		         System.exit(0);
 		      }
 
-		      System.out.println("Inserted successfully");
+		      System.out.println("Inserted successfully");*/
 		      
 	      
 		
